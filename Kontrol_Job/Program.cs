73489ab51e8708f002a435a20fbef74e80fb11c3
Adelina <dinak_83@list.ru>
2022-08-06 17:38:28 +0300

    Программа для решения задачи

---
+++ Kontrol_Job/Program.cs
@@ -0,0 +1,37 @@
+﻿
+void KontrolWork()
+{
+    Console.WriteLine("Дан массив:");
+    string[] array = { "hello", "2", "world", ":-)", "4568", "yes" };
+    PrintArray(array);
+
+    string[] newArray = new string[array.Length];
+    Console.WriteLine("Массив, где длина элементов меньше или равна 3:");
+    FillNewArray(array, newArray);
+    PrintArray(newArray);
+}
+
+void PrintArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+void FillNewArray(string[] array, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+}
+
+KontrolWork();
